9c4e2a071b61d0157558d87775a4bca8997af870
dianac22 <diana.caraguis@stud.acs.upb.ro>
2023-06-05 18:08:18 +0300

    Google Search Test - working

--- java-automation/src/test/java/Tests/GoogleUITest.java
+++ java-automation/src/test/java/Tests/GoogleUITest.java
@@ -1,7 +1,10 @@
 package Tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -17,8 +20,14 @@ public class GoogleUITest {
     }
 
     @Test
-    public void SearchOnGoogleTest() {
+    public void SearchOnGoogleTest() throws InterruptedException{
         driver = new ChromeDriver();
         driver.get("https://www.google.com/");
+
+        WebElement acceptCookiesButton = driver.findElement(By.id("L2AGLb"));
+        acceptCookiesButton.click();
+        WebElement searchInput = driver.findElement(By.name("q"));
+        searchInput.sendKeys("Google Atelierul Digital");
+        searchInput.sendKeys(Keys.ENTER);
     }
 }
